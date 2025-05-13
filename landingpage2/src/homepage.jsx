081a804f65ea373fc75c718f9c2da7465e880d12
richard kasomi <167867300+str4wh@users.noreply.github.com>
2025-05-13 14:03:04 +0300

    First Commit

--- landingpage2/src/homepage.jsx
+++ landingpage2/src/homepage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import Logo from './assets/logo.png'
+import Logo from './assets/logo.jpg'
+import MicroPic from './assets/microfinance.jpg'
+import MicroPic11 from './assets/microfinance11.jpg'
 import { FaBars, FaTimes } from "react-icons/fa";
 
 
@@ -8,7 +10,7 @@ function Homepage() {
     const[isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <nav class="bg-blue-700 text-white">
+        <nav class="bg-blue-400 text-white">
             <div class="border-0 flex items-center justify-between px-4 py-2">
                 <div class="w-full max-w-18">
                     <img class="h-10" src={Logo} alt="my logo" />
@@ -35,13 +37,36 @@ function Homepage() {
             {/* Mobile menu (visible only when isOpen is true) */}
             {isOpen && (
             <ul class="mt-4 text-center space-y-4 text-xl pb-2 md:hidden">
-                <li><a lass="hover:bg-blue-300 px-4 py-2 rounded" href="#">Home</a></li>
-                <li><a class="hover:bg-blue-300 px-4 py-2 rounded" href="#">Sales</a></li>
-                <li><a class="hover:bg-blue-300 px-4 py-2 rounded" href="#">Banking & Deposits</a></li>
-                <li><a class="hover:bg-blue-300 px-4 py-2 rounded" href="#">Mpesa & Chama</a></li>
+                <li><a class="hover:bg-blue-300 px-4 py-2 rounded transition duration-200"href="#">Home</a></li>
+                <li><a class="hover:bg-blue-300 px-4 py-2 rounded transition duration-200" href="#">Sales</a></li>
+                <li><a class="hover:bg-blue-300 px-4 py-2 rounded transition duration-200" href="#">Banking & Deposits</a></li>
+                <li><a class="hover:bg-blue-300 px-4 py-2 rounded transition duration-200" href="#">Mpesa & Chama</a></li>
             </ul>
             )}
         </nav>
+        
+        <div class="relative h-[calc(100vh-56px)]">
+            <img class="h-full w-full object-cover" src={MicroPic11} alt="micro saving pic" />
+            <div class="absolute top-0 left-0 w-full h-full   flex items-center justify-start p-4 sm:px-6">
+                <div className="text-white max-w-xl text-left">
+                    <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-blue-500">
+                        PesaPamoja
+                    </h1>
+                    <p className="mt-4 text-sm sm:text-base md:text-lg leading-relaxed">
+                        Helps small groups save, and grow through a simple, transparent platform that promotes trust, access, and empowerment.
+                    </p>
+                    <button class="border-0 bg-blue-400 hover:bg-blue-600 transition duration-200 text-white pl-2 pr-2 pt-2 pb-2 rounded-2xl mt-2">
+                        Get started
+                    </button>
+                     <button class="border-0 bg-blue-400 hover:bg-blue-600 transition duration-200 text-white pl-2 pr-2 pt-2 pb-2 rounded-2xl mt-2 ml-2">
+                        Learn more
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        
+
     </div>
   )
 }
